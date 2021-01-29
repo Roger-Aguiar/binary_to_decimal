5aeaafd5f75443d10d503192381cb31c9ffee437
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-01-29 18:54:51 -0300

    First commit: The project was created.

---
+++ BinaryToInteger.cs
@@ -0,0 +1,15 @@
+/*
+Name:         Roger Silva Santos Aguiar
+Function:     It converts an array of zero and one to its integer representation
+Initial date: January 29, 2021
+Last update:  January 29, 2021
+*/
+
+namespace OnesAndZeroes
+{
+    class BinaryToNumber
+    {
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnesAndZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
