8b33a4ba538ce4d634454dcc12d8398a6e52848e
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-01-29 19:39:10 -0300

    Function to convert from binary to integer was created.

--- BinaryToInteger.cs
+++ BinaryToInteger.cs
@@ -5,11 +5,25 @@ Initial date: January 29, 2021
 Last update:  January 29, 2021
 */
 
+using System;
+
 namespace OnesAndZeroes
 {
     class BinaryToNumber
-    {
-        
+    {       
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            int number = 0;
+            int power = BinaryArray.Length - 1;
+
+            for(int bit = 0; bit < BinaryArray.Length; bit++)
+            {
+                number = number + (Convert.ToInt32(BinaryArray[bit] * (Math.Pow(2, power))));
+                power--;
+            }
+
+            return number;          
+        }        
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿/*
+Name:         Roger Silva Santos Aguiar
+Function:     It tests the BinaryToInteger class
+Initial date: January 29, 2021
+Last update:  January 29, 2021
+*/
+using System;
 
 namespace OnesAndZeroes
 {
     class Program
     {
         static void Main(string[] args)
+        {         
+            Console.Write("Enter a binary number: ");
+            string binary = Console.ReadLine();
+
+            int [] BinaryArray = ConvertToArray(binary);            
+            int integer_number = BinaryToNumber.binaryArrayToNumber(BinaryArray);            
+            DisplayResult(integer_number, BinaryArray);
+        }
+
+        static int [] ConvertToArray(string binary)
         {
-            Console.WriteLine("Hello World!");
+            int [] BinaryArray = new int[binary.Length];
+            
+            for(int i = 0; i < binary.Length; i++)
+            {
+                BinaryArray[i] = Convert.ToInt32(binary[i].ToString());
+            }
+            return BinaryArray;
+        }
+        static void DisplayResult(int integer_number, int [] BinaryArray)
+        {
+            string output = "";
+
+            for(int i = 0; i < BinaryArray.Length; i++)
+            {
+                output += BinaryArray[i].ToString();
+            }
+            Console.WriteLine($"{output} = {integer_number}");
         }
     }
 }
